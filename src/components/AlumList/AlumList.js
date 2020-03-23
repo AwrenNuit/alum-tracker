4c8ef1436728608fb01f1b7ed7a18f0a83c87360
AwrenNuit <a.rennaker@yahoo.com>
2020-03-23 16:26:59 -0500

    set up month and week picker

--- src/components/AlumList/AlumList.js
+++ src/components/AlumList/AlumList.js
@@ -7,48 +7,111 @@ export default function AlumList() {
   // const alumList = useSelector(state => state.alum); // reducer holds alum list from firebase
   // const cohortList = useSelector(state => state.cohorts); // reducer holds cohort list from firebase
   const [alumPresent, setAlumPresent] = useState([]);
+  const [month, setMonth] = useState('');
+  const [week, setWeek] = useState('');
 
   useEffect(()=>{
-    //
+    let date = new Date().getMonth();
+    switch(+date){
+      case 0:
+        setMonth('January');
+        break;
+      case 1:
+        setMonth('February');
+        break;
+      case 2:
+        setMonth('March');
+        break;
+      case 3:
+        setMonth('April');
+        break;
+      case 4:
+        setMonth('May');
+        break;
+      case 5:
+        setMonth('June');
+        break;
+      case 6:
+        setMonth('July');
+        break;
+      case 7:
+        setMonth('August');
+        break;
+      case 8:
+        setMonth('September');
+        break;
+      case 9:
+        setMonth('October');
+        break;
+      case 10:
+        setMonth('November');
+        break;
+      case 11:
+        setMonth('December');
+        break;
+    }
   })
 
   return(
     <>
     <h1>Who Was Present?</h1>
     <form>
-      <select>
-        <option>cohort1</option>
-        <option>cohort2</option>
-      </select>
+      <div>
+        <select>
+          <option>cohort1</option>
+          <option>cohort2</option>
+        </select>
+      </div>
+
+      <div>
+        <p>
+          {month} Week&nbsp;
+          <select onChange={(e)=>setWeek(e.target.value)}>
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+          </select>
+        </p>
+      </div>
 
       {/* checkbox list of alum based on selected cohort */}
       <div>
-        <label>
-          <input type="checkbox" name="alum" /> 
-          alum 1
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" name="alum" /> 
-          alum 2
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" name="alum" /> 
-          alum 3
-        </label>
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" name="alum" /> 
-          alum 4
-        </label>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Alum Standup</th>
+              <th>Alum Scrum</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>ALUM 1</td>
+              <td>
+                <input
+                  type="checkbox"
+                  name="standup"
+                  // value={}
+                  // onChange={}
+                />
+              </td>
+              <td>
+                <input
+                  type="checkbox"
+                  name="scrum"
+                  // value={}
+                  // onChange={}
+                />
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
 
       <div>
-        <button type="submit">These Alum Were Present</button>
+        <button type="submit">Submit</button>
       </div>
     </form>
     <hr />
