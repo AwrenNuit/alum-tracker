41ea8a142cd72ac406ef2f0bd9feed5929933ea3
AwrenNuit <a.rennaker@yahoo.com>
2020-03-24 16:26:29 -0500

    set up redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App/App';
 
+const cohortListReducer = (state=[], action) => {
+  switch(action.type){
+    case `SET_COHORT_LIST`:
+      return [...state, action.payload];
+    case `CLEAR_COHORT_LIST`:
+      return [];
+    default:
+      return state;
+  }
+}
+
+const store = createStore(
+  combineReducers({
+  cohortListReducer,
+  })
+);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>, 
   document.getElementById('root')
 );
