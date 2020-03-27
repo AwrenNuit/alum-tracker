01930cb93931df20144ce7fc7519e5dd0518bca8
AwrenNuit <a.rennaker@yahoo.com>
2020-03-27 14:05:11 -0500

    imported components

--- src/components/Attendance/Attendance.js
+++ src/components/Attendance/Attendance.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import AttendanceTally from '../AttendanceTally/AttendanceTally';
+import AttendanceNames from '../AttendanceNames/AttendanceNames';
 
 export default function Attendance() {
 
