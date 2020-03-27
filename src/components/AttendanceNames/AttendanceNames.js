3559c5fd368d7b8b6c3d78b2931b67a04d2e3b67
AwrenNuit <a.rennaker@yahoo.com>
2020-03-27 14:41:43 -0500

    set up table layout

--- src/components/AttendanceNames/AttendanceNames.js
+++ src/components/AttendanceNames/AttendanceNames.js
@@ -3,7 +3,20 @@ import React from 'react';
 export default function AttendanceNames() {
 
   return(
-    <>
-    </>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Month</th>
+            <th>Week</th>
+            <th>Event</th>
+            <th>Alum Present</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+        </tbody>
+      </table>
+    </div>
   );
 }
