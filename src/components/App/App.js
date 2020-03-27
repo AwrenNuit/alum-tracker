c349e7ea2f520383e68be96544977df4ba4b2d78
AwrenNuit <a.rennaker@yahoo.com>
2020-03-27 14:03:19 -0500

    created template for component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import AddCohort from '../AddCohort/AddCohort';
 import AlumList from '../AlumList/AlumList';
+import Attendance from '../Attendance/Attendance';
 
 export default function App() {
   return (
     <>
+      <Attendance />
       <AddCohort />
       <AlumList />
     </>
--- src/components/Attendance/Attendance.js
+++ src/components/Attendance/Attendance.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Attendance() {
+
+  const [choice, setChoice] = useState('');
 
   return(
     <div className="main-container">
       <h1>Attendance List</h1>
+      
+      <div>
+        <p>Which would you like to see?</p>
+        <div>
+          <input type="radio" name="choice" value="tally" onChange={(e)=>setChoice(e.target.value)} />
+            <label>Tally</label>
+        </div>
+        <div>
+          <input type="radio" name="choice" value="names" onChange={(e)=>setChoice(e.target.value)} />
+            <label>Names</label>
+        </div>
+      </div>
+
+      {choice === 'tally' ? <AttendanceTally /> : ''}
+      {choice === 'names' ? <AttendanceNames /> : ''}
 
     </div>
   );
