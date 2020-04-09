1d419a59dcdd747d570b776ca7c8f8badaabcc1a
AwrenNuit <a.rennaker@yahoo.com>
2020-04-09 08:43:11 -0500

    creating logic for tally

--- src/components/Attendance/Attendance.js
+++ src/components/Attendance/Attendance.js
@@ -7,13 +7,14 @@ import { useSelector, useDispatch } from 'react-redux';
 export default function Attendance() {
 
   const dispatch = useDispatch();
-  // const alumPresentList = useSelector(state => state.alumPresentListReducer);
-  // const cohortPresentList = useSelector(state => state.cohortPresentListReducer);
+  // const alumalumPresentList = useSelector(state => state.alumalumPresentListReducer);
+  // const cohortalumPresentList = useSelector(state => state.cohortalumPresentListReducer);
   // const monthList = useSelector(state => state.monthListReducer);
   // const weekList = useSelector(state => state.weekListReducer);
   const allData = useSelector(state => state.allDataReducer);
   const [alum, setAlum] = useState([]);
   const [choice, setChoice] = useState('');
+  const [month, setMonth] = useState([]);
   const [tally, setTally] = useState([]);
 
   useEffect(()=>{
@@ -27,19 +28,27 @@ export default function Attendance() {
   useEffect(()=>{
     if(allData){
       let monthKey = '';
+      let monthList = [];
       let cohortKey = '';
-      let present = [];
+      let alumPresent = [];
+      let alumTally = [];
 
       for(let i=0; i<allData.length; i++) {
+        monthList.push(Object.keys(allData[i]));
         if(!monthKey.includes(allData[i])){
           monthKey = Object.keys(allData[i]);
+          // alumTally.push({[monthKey]: allData[i][monthKey]});
           for(let j=0; j<allData.length; j++) {
             cohortKey = Object.keys(allData[i][monthKey])[j];
-            present.push({[cohortKey]: allData[i][monthKey][cohortKey]});
+            alumPresent.push({[cohortKey]: allData[i][monthKey][cohortKey]});
+            // alumTally.push({[monthKey]: allData[i][monthKey][cohortKey]});
+            alumTally.push({[monthKey]: allData[i][monthList[i]][cohortKey]});
           }
         }
       }
-      setAlum(present);
+      setMonth(monthList.flat(Infinity));
+      // setAlum(alumPresent);
+      setTally(alumTally);
     }
   }, [allData]);
 
@@ -47,19 +56,13 @@ export default function Attendance() {
     <div className="main-container">
       <h1>Attendance List</h1>
 
-      {JSON.stringify(allData)}
+      {/* {JSON.stringify(allData)}
       <br />
       <br />
       {JSON.stringify(alum)}
-
-      {/* {Object.entries(allData).map((m,i)=>
-        <div key={i}>{m}</div>
-      )} */}
-      {/* {allData && allData[0] ? allData[0].map((m,i)=>
-        <div key={i}>{m}</div>
-      )
-    :
-    ''} */}
+      <br />
+      <br />
+      {JSON.stringify(tally)} */}
       
       <div>
         <p>Which would you like to see?</p>
@@ -73,11 +76,7 @@ export default function Attendance() {
         </div>
       </div>
 
-      {/* {allData.map((m,i)=>
-        <div key={i}>{m}</div>
-      )} */}
-
-      {choice === 'tally' ? <AttendanceTally /> : ''}
+      {choice === 'tally' ? <AttendanceTally month={month} tally={tally} /> : ''}
       {choice === 'names' ? <AttendanceNames /> : ''}
 
     </div>
--- src/components/AttendanceTally/AttendanceTally.js
+++ src/components/AttendanceTally/AttendanceTally.js
@@ -1,22 +1,65 @@
 import React from 'react';
+import { Fragment } from 'react';
 
-export default function AttendanceTally() {
+export default function AttendanceTally(props) {
+
+  const getTallyTotal = () => {
+    console.log('in there');
+    let prevMonth = '';
+    let alum = [];
+    let total = 0;
+    for(let i=0; i<props.tally.length; i++){
+      let key = Object.keys(props.tally[i]).toString();
+      console.log(' ');
+      console.log(' ');
+      console.log(`-------------- loop ${i} --------------`);
+      if(alum.length < 1){
+        console.log('-------------- in < 1 --------------');
+        console.log('object keys:', key);
+        alum.push(props.tally[i][key]);
+        prevMonth = key;
+      }
+      else if(prevMonth === key){
+        // put loop in here that runs while prevMonth = key
+        // once it is NOT equal to key, reset alum & return total
+        console.log('-------------- prevMonth --------------');
+        console.log('prevMonth:', prevMonth);
+        alum.push(props.tally[i][key]);
+        console.log('alum:', alum.flat(Infinity));
+        total = alum.flat(Infinity).length;
+        alum = [];
+      }
+    }
+    console.log('total:', total);
+  }
 
   return(
     <div>
+      {JSON.stringify(props.tally)}
+      {getTallyTotal()}
       <table>
         <thead>
           <tr>
-            <th>Month & Week</th>
-            <th>Month & Week</th>
-          </tr>
-          <tr>
-            <th>Alumni Standup</th>
-            <th>Alum Scrum</th>
+          <th>Month & Week</th>
+          <th>Event</th>
+          <th>Attendance</th>
           </tr>
         </thead>
         <tbody>
-          
+          {props.month.map((month, i)=>
+          <Fragment key={i}>
+            <tr>
+              <td>{month.replace(/_/g, " ")}</td>
+              <td>Standup</td>
+              <td># alum present</td>
+            </tr>
+            <tr>
+              <td>{month.replace(/_/g, " ")}</td>
+              <td>Scrum</td>
+              <td># alum present</td>
+            </tr>
+          </Fragment>
+          )}
         </tbody>
       </table>
     </div>
