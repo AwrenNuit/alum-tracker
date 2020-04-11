4f23cb5e2a92fa87448c5dc36260aa393516241b
AwrenNuit <a.rennaker@yahoo.com>
2020-04-10 19:57:53 -0500

    created basic navbar, added routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
-// import AddCohort from '../AddCohort/AddCohort';
-// import AlumList from '../AlumList/AlumList';
+import AddCohort from '../AddCohort/AddCohort';
+import AlumList from '../AlumList/AlumList';
 import Attendance from '../Attendance/Attendance';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 export default function App() {
   return (
-    <>
-      <Attendance />
-      {/* <AddCohort />
-      <AlumList /> */}
-    </>
+    <Router>
+      <nav>
+        <Link class="link" to="/">Home</Link>
+        <Link class="link" to="/present">Who Was There?</Link>
+        <Link class="link" to="/add-cohort">Add a Cohort</Link>
+      </nav>
+      <Route exact path="/" component={Attendance} />
+      <Route exact path="/add-cohort" component={AddCohort} />
+      <Route exact path="/present" component={AlumList} />
+    </Router>
   );
 }
