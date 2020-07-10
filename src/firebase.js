f2db10602eec0bbf4122707f8565be70ef0fbf4f
AwrenNuit <a.rennaker@yahoo.com>
2020-07-10 18:32:07 -0500

    modified gitignore, imported database url

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,11 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
+import { DATABASE_URL } from './.env.js';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: "https://alum-tracker.firebaseio.com", // why can't it read from .env?
+  databaseURL: DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
