d1ea12bea67db95302e8da71020cf1bae651a791
AwrenNuit <a.rennaker@yahoo.com>
2020-07-10 18:47:16 -0500

    set up content grid

--- src/components/AddCohort/AddCohort.js
+++ src/components/AddCohort/AddCohort.js
@@ -1,22 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { db } from '../../firebase';
-import './AddCohort.css';
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { db } from "../../firebase";
+import "./AddCohort.css";
 
 export default function AddCohort() {
-
   const dispatch = useDispatch();
-  const cohortList = useSelector(state => state.cohortListReducer);
+  const cohortList = useSelector((state) => state.cohortListReducer);
   const [count, setCount] = useState(1);
-  const [graduationDate, setGraduationDate] = useState('');
-  const [newCohort, setNewCohort] = useState('');
-  const [newStudent, setNewStudent] = useState('');
+  const [graduationDate, setGraduationDate] = useState("");
+  const [newCohort, setNewCohort] = useState("");
+  const [newStudent, setNewStudent] = useState("");
   const [studentList, setStudentList] = useState([]);
 
-  useEffect(()=>{
-    db.ref('cohorts').once(`value`, snap => {
-      snap.forEach(child => {
-        dispatch({type: `SET_COHORT_LIST`, payload: child.key});
+  useEffect(() => {
+    db.ref("cohorts").once(`value`, (snap) => {
+      snap.forEach((child) => {
+        dispatch({ type: `SET_COHORT_LIST`, payload: child.key });
         // console.log('db child:', child.key); // For cohort list by key
       });
     });
@@ -27,99 +26,105 @@ export default function AddCohort() {
     //     console.log('db child:', child.val()); // for data in this document
     //   });
     // });
-    
   }, []);
 
-  useEffect(()=>{
-    if(studentList[0] === ''){
+  useEffect(() => {
+    if (studentList[0] === "") {
       setStudentList([newStudent]);
-    }
-    else {
+    } else {
       setStudentList([...studentList, newStudent]);
     }
-    setNewStudent('');
+    setNewStudent("");
   }, [count]);
 
   const addStudentInput = () => {
     let output = [];
-    for(let i=1; i<=count; i++){
-      output.push(<div className="add-student" key={i}>
-                    <label>Student #{i}: </label>
-                    <input 
-                      type="text" 
-                      value={studentList[i-1] || newStudent}
-                      onChange={(e)=>setNewStudent(e.target.value)}
-                      placeholder="student name"
-                      autoFocus
-                    />
-                  </div>);
+    for (let i = 1; i <= count; i++) {
+      output.push(
+        <div className="add-student cohort-grid" key={i}>
+          <label>Student #{i}: </label>
+          <input
+            type="text"
+            value={studentList[i - 1] || newStudent}
+            onChange={(e) => setNewStudent(e.target.value)}
+            placeholder="student name"
+            autoFocus
+          />
+        </div>
+      );
     }
     return output;
-  }
+  };
 
   const checkAgainstList = () => {
-    if(cohortList.includes(newCohort)){
+    if (cohortList.includes(newCohort)) {
       alert(`That cohort has already been added.`);
-      setNewCohort('');
+      setNewCohort("");
     }
-  }
+  };
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if(newCohort !== '' && graduationDate !== '' && studentList !== ''){
+    if (newCohort !== "" && graduationDate !== "" && studentList !== "") {
       db.ref(`/cohorts/${newCohort}`).set({
         graduation: graduationDate,
-        studentList
+        studentList,
       });
       // clearReducer();
-      setGraduationDate('');
-      setNewCohort('');
-      setNewStudent('');
-      setStudentList('');
+      setGraduationDate("");
+      setNewCohort("");
+      setNewStudent("");
+      setStudentList("");
       setCount(1);
     }
-  }
+  };
 
-  return(
+  return (
     <div className="main-container">
       <h1>Add a Cohort</h1>
 
       <div className="add-directions-container">
         <div className="add-directions">
-          <p style={{fontWeight:'bold'}}>To use:</p>
+          <p style={{ fontWeight: "bold" }}>To use:</p>
           <div className="add-directions-list">
             <ul>
               <li>Enter the name of the cohort you wish to add</li>
               <li>Select the date that they graduate</li>
-              <li>Enter a student's name, then click the "Save & Add Another Student" button to add it to the list</li>
-              <li>Once you have all the students (there will be a blank input at the end) you can hit the "Submit" button</li>
+              <li>
+                Enter a student's name, then click the "Save & Add Another
+                Student" button to add it to the list
+              </li>
+              <li>
+                Once you have all the students (there will be a blank input at
+                the end) you can hit the "Submit" button
+              </li>
               <li>Rinse & repeat for each cohort you want to add :)</li>
             </ul>
           </div>
-          <div style={{marginTop:'20px'}}>
-            <p style={{fontWeight:'bold'}}>Current students:&nbsp;</p> 
-            {studentList.length !== 0 ? studentList.join(', ') : ''}
+          <div style={{ marginTop: "20px" }}>
+            <p style={{ fontWeight: "bold" }}>Current students:&nbsp;</p>
+            {studentList.length !== 0 ? studentList.join(", ") : ""}
           </div>
         </div>
       </div>
-      
+
       <form onSubmit={handleSubmit}>
-        <div className="add-cohort">
+        <div className="add-cohort cohort-grid">
           <label>Cohort Name: </label>
-          <input 
-            type="text" 
-            value={newCohort} 
-            onChange={(e)=>setNewCohort(e.target.value)} 
+          <input
+            type="text"
+            value={newCohort}
+            onChange={(e) => setNewCohort(e.target.value)}
             onBlur={checkAgainstList}
-            placeholder="cohort name" 
+            placeholder="cohort name"
           />
         </div>
-        <div className="add-graduation">
+        <div className="add-graduation cohort-grid">
           <label>Graduation Date: </label>
-          <input 
-            type="date" 
-            value={graduationDate} 
-            onChange={(e)=>setGraduationDate(e.target.value)} 
+          <input
+            type="date"
+            value={graduationDate}
+            onChange={(e) => setGraduationDate(e.target.value)}
           />
         </div>
 
@@ -127,9 +132,17 @@ export default function AddCohort() {
         {addStudentInput()}
 
         {/* display msg that student was saved, disable input field */}
-        <button className="add-student-btn" type="button" onClick={()=>setCount(count+1)}>Save & Add Another Student</button>
+        <button
+          className="add-student-btn"
+          type="button"
+          onClick={() => setCount(count + 1)}
+        >
+          Save & Add Another Student
+        </button>
         <div>
-          <button className="submit-btn" type="submit">Submit</button>
+          <button className="submit-btn" type="submit">
+            Submit
+          </button>
         </div>
       </form>
     </div>
